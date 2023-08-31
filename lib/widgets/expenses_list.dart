35e48041efbf0e9eec026748401360eca9e5bc4c
mikol <mikolajek.a@gmail.com>
2023-08-31 16:58:32 +0200

    Module 120 - fullscreen modal, dismissible to remove items added

--- lib/widgets/expenses_list.dart
+++ lib/widgets/expenses_list.dart
@@ -3,15 +3,19 @@ import 'package:tcg_expenses_app/models/expense.dart';
 import 'package:tcg_expenses_app/widgets/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({required this.expenses, Key? key}) : super(key: key);
+  const ExpensesList({required this.expenses, required this.onRemoveExpense, Key? key}) : super(key: key);
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => ExpenseItem(expense: expenses[index]),
+      itemBuilder: (context, index) => Dismissible(
+          key: ValueKey(expenses[index]),
+          onDismissed: (direction) {onRemoveExpense(expenses[index]);},
+          child: ExpenseItem(expense: expenses[index])),
     );
   }
 }
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:tcg_expenses_app/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({Key? key}) : super(key: key);
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
@@ -53,6 +55,12 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return;
     }
+    widget.onAddExpense(Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory));
+    Navigator.pop(context);
   }
 
   @override
@@ -65,7 +73,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16.0),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: <Widget>[
           TextField(
