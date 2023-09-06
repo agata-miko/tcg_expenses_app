5ef87f175c44a01b596933ad58c280ca790afd6c
mikol <mikolajek.a@gmail.com>
2023-09-06 15:23:19 +0200

    Module 125 - theme added

--- lib/widgets/expenses_list.dart
+++ lib/widgets/expenses_list.dart
@@ -3,7 +3,9 @@ import 'package:tcg_expenses_app/models/expense.dart';
 import 'package:tcg_expenses_app/widgets/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({required this.expenses, required this.onRemoveExpense, Key? key}) : super(key: key);
+  const ExpensesList(
+      {required this.expenses, required this.onRemoveExpense, Key? key})
+      : super(key: key);
 
   final List<Expense> expenses;
   final void Function(Expense expense) onRemoveExpense;
@@ -14,7 +16,15 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (context, index) => Dismissible(
           key: ValueKey(expenses[index]),
-          onDismissed: (direction) {onRemoveExpense(expenses[index]);},
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.5),
+            margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+                vertical: Theme.of(context).cardTheme.margin!.vertical),
+          ),
+          onDismissed: (direction) {
+            onRemoveExpense(expenses[index]);
+          },
           child: ExpenseItem(expense: expenses[index])),
     );
   }
