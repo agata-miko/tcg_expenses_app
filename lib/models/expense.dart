a3239b6b4e1701039cb7d80d037b8ec4f9d44ef6
mikol <mikolajek.a@gmail.com>
2023-09-06 19:26:22 +0200

    Module 128 - ExpenseBucket class added

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -13,13 +13,14 @@ Map<Category, IconData> categoryIcons = {
   Category.travel: Icons.flight_takeoff,
   Category.leisure: Icons.movie,
   Category.work: Icons.work,
-
 };
 
-
-
 class Expense {
-  Expense({required this.title, required this.amount, required this.date, required this.category})
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
       : id = uuid.v4();
 
   final String id;
@@ -30,5 +31,27 @@ class Expense {
 
   get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
